09706b9852de62ce7fc4c115613b8a15aaf62242
Mike Steinert <mike.steinert@gmail.com>
2011-08-28 19:29:25 -0600

    Moving upstairs...
    Signed-off-by: Mike Steinert <mike.steinert@gmail.com>

---
+++ moka/lang/lang.h
@@ -0,0 +1,64 @@
+// Copyright 2011 Michael Steinert. All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided
+//       with the distribution.
+//     * The names of the copyright holder, the author, nor any contributors
+//       may be used to endorse or promote products derived from this
+//       software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef MOKA_LANG_H
+#define MOKA_LANG_H
+
+#include "moka/module.h"
+
+namespace moka {
+
+class Exception;
+
+} // namespace moka
+
+class moka::Binary {
+public:
+  static v8::Handle<v8::Value> New(
+      v8::Handle<v8::Value> message = v8::Handle<v8::Value>());
+
+  static v8::Handle<v8::FunctionTemplate> GetTemplate();
+
+protected: // V8 interface methods
+  static v8::Handle<v8::Value> New(const v8::Arguments& arguments);
+
+  static v8::Handle<v8::Value> ToString(const v8::Arguments& arguments);
+
+protected: // Protected methods
+  Exception() {}
+
+  virtual ~Exception() {}
+
+private: // Private methods
+  Exception(Binary const& that);
+
+  void operator=(Exception const& that);
+};
+
+#endif // MOKA_LANG_H
+
+// vim: tabstop=2:sw=2:expandtab
